71af403ca2ead39ac1d12bab82341941ada82873
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-08-03 09:10:50 -0300

    add: tailwind

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import store from './redux/store';
 import App from './App';
 
