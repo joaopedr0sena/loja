b06bfca2e9f701b047998b9bf5d5fcfc8864adfa
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-08-05 08:07:32 -0300

    add: width in home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main>
+      <main className="w-2/4 max-w-2xl w-full m-auto">
         <ProductsList />
       </main>
     </>
