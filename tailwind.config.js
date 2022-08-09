80358fbc80160a0f34d8c9a6ddec47f4fea1d082
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-08-09 09:14:04 -0300

    create: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: '#FFFF00',
+      secondary: '#153243',
+      tertiary: '#FF0035',
+      quaternary: '#06D6A0',
+      quinary: '#e7e3e2',
+      white: '#ffffff',
+    },
     extend: {},
   },
   variants: {
