8ff39814acc849ca82879764f9af1b1b27375878
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-08-09 09:20:32 -0300

    update: header

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../../atoms/search-bar';
+import Title from '../../atoms/title';
 import ListCategories from '../../molecules/list-categories';
 
 export default function Header({ title = 'Loja' }) {
   return (
-    <header>
-      <h1>{title}</h1>
-      <SearchBar />
-      <nav>
-        <ul>
+    <header className="bg-primary w-full flex flex-col justify-between">
+      <div className="flex items-center">
+        <div>
+          <Title>{title}</Title>
+        </div>
+        <div className="w-3/5">
+          <SearchBar />
+        </div>
+      </div>
+      <nav className="my-2">
+        <ul className="flex flex-row justify-between">
           <li>
             <Link to="/shoppingCart">
               carrinho
