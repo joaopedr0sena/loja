2e39b96da6b09bfc9e971b7133ce52dff5266a1a
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-08-09 09:24:03 -0300

    styled: item-products-lit

--- src/components/molecules/item-products-list/index.js
+++ src/components/molecules/item-products-list/index.js
@@ -11,9 +11,9 @@ export default function ItemProductsList({
   thumbnail,
 }) {
   return (
-    <li key={id} className="flex justify-between">
-      <Link to={`/description/product/${id}`}>
-        <Thumbnail img={thumbnail} title={title} />
+    <li key={id} className="bg-quinary shadow-md flex flex-col justify-between my-2 rounded-xl w-56 max-w-sm h-96">
+      <Thumbnail img={thumbnail} title={title} />
+      <Link to={`/description/product/${id}`} className="w-full flex flex-col justify-between ml-px">
         <ListItemTitle>{title}</ListItemTitle>
         <p>{`R$ ${price}`}</p>
       </Link>
