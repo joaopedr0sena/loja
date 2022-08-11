699a52f87f72abdaded3313f7bac27d6ab6e910c
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-08-11 13:19:38 -0300

    change: font size at title

--- src/components/atoms/title/index.js
+++ src/components/atoms/title/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Title({ children }) {
   return (
-    <h1 className="text-6xl mr-14">
+    <h1 className="text-4xl mr-14">
       {children}
     </h1>
   );
