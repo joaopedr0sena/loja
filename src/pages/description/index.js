71a609ae54f31f880d56c5c2628d0ff4b86bfaf5
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-08-19 09:49:21 -0300

    fix & add: description & gallery

--- src/pages/description/index.js
+++ src/pages/description/index.js
@@ -6,6 +6,7 @@ import ButtonAddCart from '../../components/atoms/button-add-cart';
 import Header from '../../components/organisms/header';
 import Subtitle from '../../components/atoms/subtitle';
 import Price from '../../components/atoms/price';
+import Gallery from '../../components/molecules/gallery';
 
 export default function Description({ match: { params: { id } } }) {
   const [loading, setLoading] = useState(true);
@@ -14,13 +15,14 @@ export default function Description({ match: { params: { id } } }) {
   const [description, setDescription] = useState({});
 
   useEffect(() => {
+    setLoading(true);
     const getInfos = async () => {
       const {
         description: newDescription,
         information: newInformation,
       } = await getDescription(id);
 
-      const specificCategory = getSpecificCategory(newInformation.category_id);
+      const specificCategory = await getSpecificCategory(newInformation.category_id);
 
       if (specificCategory && newDescription && newInformation) {
         setCategory(specificCategory);
@@ -31,30 +33,28 @@ export default function Description({ match: { params: { id } } }) {
     };
 
     getInfos();
-  }, [category, description, id, information]);
+  }, [id]);
 
   if (loading) return (<p>...</p>);
   return (
-    <div>
+    <div className="bg-quinary pb-10">
       <Header title="Descrição" />
-      <Subtitle>{information.title}</Subtitle>
-      {information.pictures.map(({
-        secure_url: secureUrl,
-        id: pictureId,
-      }) => (
-        <img
-          src={secureUrl}
-          alt={`${information.title}`}
-          key={`${pictureId}`}
-          width="100px"
-        />
-      ))}
-      <Price>{information.price}</Price>
-      <ButtonAddCart itemId={information.id} />
-      <div className="description">
-        <p>{description.plain_text}</p>
+      <section className="flex flex-row mx-auto mt-5 py-5 px-10 max-w-6xl bg-white rounded-md">
+        <div className="w-1/2">
+          <Gallery images={information.pictures} />
+          <Price>{information.price}</Price>
+          <ButtonAddCart itemId={information.id} />
+        </div>
+        <div className="w-1/2">
+          <Subtitle>{information.title}</Subtitle>
+          <div className="mt-8">
+            <p>{description.plain_text}</p>
+          </div>
+        </div>
+      </section>
+      <div className="w-11/12 mx-auto">
+        {category && <ProductsList category={category} noId={information.id} type="recommended" />}
       </div>
-      <ProductsList category={category} noId={information.id} />
     </div>
   );
 }
