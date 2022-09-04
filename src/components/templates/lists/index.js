4f4baab4101e9c0354a71e50a3e69528a587a1c3
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-09-04 16:14:42 -0300

    update: background color

--- src/components/templates/lists/index.js
+++ src/components/templates/lists/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function ListsTemplate({ header, subtitle, list }) {
   return (
-    <>
+    <div className="bg-quinary">
       {header}
       {subtitle && subtitle}
       <div className="flex justify-center">
         {list}
       </div>
-    </>
+    </div>
   );
 }
