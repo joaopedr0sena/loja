52177d56917f0ffeab43a3620054fde90d57b3bb
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-10-10 16:17:53 -0300

    update: gallery

--- src/components/molecules/gallery/index.js
+++ src/components/molecules/gallery/index.js
@@ -4,36 +4,34 @@ export default function Gallery({ images }) {
   const [showImage, setShowImage] = useState(images[0].secure_url);
 
   return (
-    <div className="flex flex-row-reverse w-full">
+    <div className="flex flex-col w-full">
       <img
-        className="h-96 w-min max-h-80 mx-auto"
+        className="max-h-96 w-min mx-auto"
         alt="product_image"
         src={showImage}
       />
-      <div className="flex flex-col overflow-x-auto max-h-96">
-        <ul>
-          {images.map(({
-            secure_url: secureUrl,
-            id: pictureId,
-          }) => (
-            <li
-              key={pictureId}
-              className={`cursor-pointer border-2 border-${showImage === secureUrl ? 'black' : 'quinary'} flex-none`}
-              onClick={() => setShowImage(secureUrl)}
-              role="presentation"
-            >
-              <div className="w-20 h-20">
-                <img
-                  className="h-full mx-auto"
-                  alt="product_image"
-                  src={secureUrl}
-                  key={`${pictureId}`}
-                />
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul className="flex flex-row overflow-x-auto w-min max-w-full mx-auto">
+        {images.map(({
+          secure_url: secureUrl,
+          id: pictureId,
+        }) => (
+          <li
+            key={pictureId}
+            className={`cursor-pointer border-2 border-${showImage === secureUrl ? 'black' : 'quinary'}`}
+            onClick={() => setShowImage(secureUrl)}
+            role="presentation"
+          >
+            <div className="w-20 h-20">
+              <img
+                className="h-full mx-auto"
+                alt="product_image"
+                src={secureUrl}
+                key={`${pictureId}`}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
