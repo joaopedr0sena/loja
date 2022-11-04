04186e07e0a46ef0fdd552dd2b93b3cd200af1ad
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-11-04 08:21:58 -0300

    update: price

--- src/components/atoms/price/index.js
+++ src/components/atoms/price/index.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function Price({ children }) {
   return (
     <p className="font-medium text-2xl">
-      <span className="text-tertiary">{'R$ '}</span>
-      {children.toLocaleString('pt-br', { currency: 'BRL' })}
+      {
+        new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(children)
+      }
     </p>
   );
 }
