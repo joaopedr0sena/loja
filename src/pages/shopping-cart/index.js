1d9b950c715bebdce1c37e603cf232cd3f5259ce
joaopedr0sena <joaopedrosena1997@gmail.com>
2023-01-07 13:44:43 -0300

    update: cart

--- src/pages/shopping-cart/index.js
+++ src/pages/shopping-cart/index.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import shoppingCartThunk from '../../redux/thunk/shoppingCart';
 import ShoppingCartList from '../../components/organisms/shopping-cart-list';
 import DefaultPage from '../../components/templates/default-page/inde';
+import Price from '../../components/atoms/price';
 
 export default function ShoppingCart() {
   const dispatch = useDispatch();
@@ -12,6 +13,7 @@ export default function ShoppingCart() {
   const listInformations = useSelector(({ shoppingCartState }) => (
     shoppingCartState.shoppingCartListInformations
   ));
+  const total = listInformations.map(({ amount, infos }) => amount * infos.information.price);
 
   useEffect(() => {
     const data = () => {
@@ -26,8 +28,18 @@ export default function ShoppingCart() {
 
   return (
     <DefaultPage loading={!cartList}>
-      <div className="max-w-4xl w-full h-9/12 mt-6 mx-auto bg-white rounded mb-10">
-        <ShoppingCartList list={listInformations} />
+      <div className="shadow-xl max-w-screen-lg mx-auto">
+        <div className="max-w-4xl w-full h-9/12 mt-6 mx-auto bg-white rounded mb-10">
+          <ShoppingCartList list={listInformations} />
+        </div>
+        <div className="text-center w-full mb-4">
+          <h2>
+            <span className="text-xl mr-1">Total:</span>
+            <Price>
+              {total && total.reduce((acc, cur) => acc + cur, 1)}
+            </Price>
+          </h2>
+        </div>
       </div>
     </DefaultPage>
   );
