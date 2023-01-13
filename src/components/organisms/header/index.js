adfad7fd77aaeb2f814fda9b56918819c82f6b3d
joaopedr0sena <joaopedrosena1997@gmail.com>
2023-01-12 22:57:51 -0300

    update: header

--- src/components/organisms/header/index.js
+++ src/components/organisms/header/index.js
@@ -8,30 +8,29 @@ import NavItem from '../../atoms/nav-item';
 export default function Header() {
   return (
     <header className="bg-secondary text-primary shadow-lg sticky top-0 left-0 w-full flex flex-col justify-between py-2">
-      <div className="flex items-center justify-around mb-3">
+      <div className="flex items-center justify-around items-center mb-3">
         <div>
           <Link to="/">
-            <h1 className="flex items-center text-xl">
+            <h1 className="flex items-center text-2xl">
               <RiShoppingBag2Fill color="white" />
               <span className="text-primary">Marketplace</span>
             </h1>
           </Link>
         </div>
-        <div className="mx-2">
+        <div className=" ml-1 mr-2 w-8/12">
           <SearchBar />
         </div>
-        <div className="text-4xl">
+        <div className="text-3xl">
           <Link to="/shoppingCart">
             <RiShoppingCart2Fill />
           </Link>
         </div>
       </div>
       <nav className="mt-2 h-6">
-        <ul className="grid grid-cols-4 h-full">
-          <NavItem to="/shoppingCart">carrinho</NavItem>
-          <NavItem to="/">produtos</NavItem>
+        <ul className="grid grid-cols-3 h-full">
+          <NavItem to="/">PRODUTOS</NavItem>
           <li className="h-full flex justify-center hover:underline"><ListCategories /></li>
-          <NavItem to="#">sobre</NavItem>
+          <NavItem to="#">SOBRE</NavItem>
         </ul>
       </nav>
     </header>
