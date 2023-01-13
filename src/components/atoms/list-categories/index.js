f0f8aef74401fc212b7929c6f0d600c453225161
joaopedr0sena <joaopedrosena1997@gmail.com>
2023-01-12 22:56:33 -0300

    update: list of categories

--- src/components/atoms/list-categories/index.js
+++ src/components/atoms/list-categories/index.js
@@ -4,6 +4,7 @@ import getCategories from '../../../utils/apis/getCategories';
 
 export default function ListCategories() {
   const [categories, setCategories] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const getApi = async () => {
@@ -14,21 +15,36 @@ export default function ListCategories() {
   }, []);
 
   return (
-    <details className="relative cursor-pointer w-max">
-      <summary className="list-none">
-        categorias
-      </summary>
-      <ul className="absolute left-0 w-min bg-white h-96 overflow-x-hidden overflow-y-auto shadow-xl rounded-lg">
-        {
-          categories.map(({ id, name }) => (
-            <li key={id} className="hover:bg-quinary text-secondary hover:underline border-quinary py-2 border-b-2">
-              <Link className="my-3" to={`/category/${id}/categoryName/${name}`}>
-                <p>{name}</p>
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
-    </details>
+    <div className="relative cursor-pointer w-max">
+      <button
+        type="button"
+        className="list-none font-bold text-sm"
+        onClick={() => setIsOpen(!isOpen)}
+        onBlur={() => {
+          setTimeout(() => {
+            setIsOpen(!isOpen);
+          }, 500);
+        }}
+      >
+        CATEGORIAS
+      </button>
+      {isOpen && (
+        <ul className="absolute left-0 top-8 w-min bg-white h-96 overflow-x-hidden overflow-y-auto shadow-xl rounded-lg">
+          {
+            categories.map(({ id, name }) => (
+              <li key={id} className="hover:bg-quinary text-center text-secondary font-bold border-quinary py-2 border-b-2">
+                <Link
+                  className="my-3"
+                  onClick={() => setIsOpen(!isOpen)}
+                  to={`/category/${id}/categoryName/${name}`}
+                >
+                  <p>{name}</p>
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+      )}
+    </div>
   );
 }
